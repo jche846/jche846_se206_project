5912bb06a106a3fcfd63f06ae258c198363aaecb
jche846 <jche846@aucklanduni.ac.nz>
2013-10-22 15:43:55 +1300

    fixed the problem with edit text boxes and its ID from xml

--- src/jche846/softeng206/contactsmanager/Add_new_contact.java
+++ src/jche846/softeng206/contactsmanager/Add_new_contact.java
@@ -6,23 +6,50 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Add_new_contact extends Activity {
+	DatabaseHandler db = new DatabaseHandler(this);
+	
 	//create button objects
 	private Button buttonAdd;
 	private Button buttonCancel;
+	
+	//edit text boxes
+	private EditText addFirst;
+	private EditText addLast;
+	private EditText addMobile;
+	private EditText addHome;
+	private EditText addWork;
+	private EditText addEmail;
+	private EditText addHomeAdd;
+	private EditText addDOB;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_new_contact);
 		
+		//Set the edit text boxes
+		addFirst = (EditText)findViewById(R.id.addText1);
+		addLast = (EditText)findViewById(R.id.addText2);
+		addMobile = (EditText)findViewById(R.id.addText3);
+		addHome = (EditText)findViewById(R.id.addText4);
+		addWork = (EditText)findViewById(R.id.addText5);
+		addEmail = (EditText)findViewById(R.id.addText6);
+		addHomeAdd = (EditText)findViewById(R.id.addText7);
+		addDOB = (EditText)findViewById(R.id.addText8);
+		
+		
 		//Set the add button to change activity on click
-        buttonAdd= (Button)findViewById(R.id.addContact);
+        buttonAdd = (Button)findViewById(R.id.addContact);
         buttonAdd.setOnClickListener(new View.OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
+				
+				
 				
 				Intent intent= new Intent();
         		intent.setClass(Add_new_contact.this, MainActivity.class);
